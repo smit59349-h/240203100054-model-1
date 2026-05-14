765fc3cbbd836dfdbd36bb2773cf812c0a420af1
AkshayThakur231 <2402031000044@silveroakuni.ac.in>
2026-05-14 23:53:22 +0530

    Added Products, Login/Register Page in Static Website

--- Day-3/Static Website/script.js
+++ Day-3/Static Website/script.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
     $("#header1").load("header.html");
     $("#footer1").load("footer.html");
 });
+
+
+
+const closeBtn = document.querySelector(".close-btn");
+
+closeBtn.addEventListener("click", () => {
+    window.close();
+});
