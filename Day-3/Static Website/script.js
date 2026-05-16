4434739bf739946e6151d3ac01523fea421a6842
AkshayThakur231 <2402031000044@silveroakuni.ac.in>
2026-05-16 14:17:32 +0530

    Edited Static Website Add To Cart Function of

--- Day-3/Static Website/script.js
+++ Day-3/Static Website/script.js
@@ -10,3 +10,31 @@ const closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener("click", () => {
     window.close();
 });
+
+
+
+
+
+function add_item() {
+    let quantitySpan = document.getElementById("pro1").innerText;
+    let quantity = parseInt(quantitySpan);
+    quantity++;
+    document.getElementById("pro1").innerText = quantity;
+    document.getElementById("total-am1").innerText = "$" + (quantity * 300) + ".0";
+
+}
+
+function min_item() {
+    let quantitySpan = document.getElementById("pro1").innerText;
+    let quantity = parseInt(quantitySpan);
+    if (quantity > 1) {
+        quantity--;
+        document.getElementById("pro1").innerText = quantity;
+        document.getElementById("total-am1").innerText = "$" + (quantity * 300) + ".0";
+        document.getElementById("totalMan2").innerText = "$" + (quantity * 300) + ".0";
+    }
+}
+
+function remove_item() {
+    document.getElementById("row1").remove();
+}
